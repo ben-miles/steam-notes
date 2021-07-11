ed0df0c9c55a5d0311041e18cb3022442791442f
Ben Miles <bcgm3studios@gmail.com>
2021-07-11 00:12:12 -0400

    Move existing JS into Vue

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,3 @@
-var app = new Vue({
-    el: '#app',
-    data: {
-        message: 'Hello Vue!'
-    }
-})
-
 let games = [
     {
         title: "No Man's Sky",
@@ -98,103 +91,100 @@ let games = [
         links: []
     }
 ];
-let gamesContainer = document.getElementsByClassName("games")[0];
-for(let i = 0; i < games.length; i++) {
 
-    let game = document.createElement("div");
-    game.className = "game";
-    game.id = games[i].title.toLowerCase().replace(/[ ':]/g, "");
-    gamesContainer.appendChild(game);
+var app = new Vue({
+    el: '#app',
+    data: {
+        games: games
+    },
+    methods:{
+        getSteamData: function(endpoint, steam_user_id = "76561197995679405") {
+            var request = new XMLHttpRequest();
+            request.open("POST", "/assets/proxy.php", true);
+            request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+            request.responseType = "json";
+            request.send("endpoint=" + endpoint + "&steam_user_id=" + steam_user_id);
+            request.onreadystatechange = function() {
+                if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+                    // TODO: If not JSON (if first char != "{"), throw unexepected result error
+                    switch(endpoint) {
+                        case "GetPlayerSummaries":
+                            var data = this.response.response.players;
+                            break;
+                        case "GetUserStatsForGame":
+                            var data = this.response.playerstats;
+                            break;
+                        case "GetOwnedGames":
+                            var data = this.response.response.games;
+                            break;
+                        case "GetRecentlyPlayedGames":
+                            var data = this.response.response.games;
+                            break;
+                    }
+                    console.log(endpoint, data);
+                }
+                // TODO: else, throw connection error
+            };
+        },
+        buildSteamGamesList: function() {
+            let gamesContainer = document.getElementsByClassName("games")[0];
+            for(let i = 0; i < games.length; i++) {
 
-    let coverLink = document.createElement("a");
-    coverLink.className = "cover";
-    coverLink.href = `https://store.steampowered.com/app/${games[i].steamId}/`;
-    coverLink.target = "_blank"
-    game.appendChild(coverLink);
+                let game = document.createElement("div");
+                game.className = "game";
+                game.id = games[i].title.toLowerCase().replace(/[ ':]/g, "");
+                gamesContainer.appendChild(game);
 
-    let coverImage = document.createElement("img");
-    coverImage.src = `https://cdn.cloudflare.steamstatic.com/steam/apps/${games[i].steamId}/header.jpg`;
-    coverLink.appendChild(coverImage);
+                let coverLink = document.createElement("a");
+                coverLink.className = "cover";
+                coverLink.href = `https://store.steampowered.com/app/${games[i].steamId}/`;
+                coverLink.target = "_blank"
+                game.appendChild(coverLink);
 
-    let title = document.createElement("h2");
-    title.className = "title";
-    title.innerHTML = games[i].title;
-    game.appendChild(title);
+                let coverImage = document.createElement("img");
+                coverImage.src = `https://cdn.cloudflare.steamstatic.com/steam/apps/${games[i].steamId}/header.jpg`;
+                coverLink.appendChild(coverImage);
 
-    let links = document.createElement("ul");
-    game.appendChild(links);
+                let title = document.createElement("h2");
+                title.className = "title";
+                title.innerHTML = games[i].title;
+                game.appendChild(title);
 
-    for(let ii = 0; ii < games[i].links.length; ii++) {
+                let links = document.createElement("ul");
+                game.appendChild(links);
 
-        let link = document.createElement("li");
-        links.appendChild(link);
+                for(let ii = 0; ii < games[i].links.length; ii++) {
 
-        let url = document.createElement("a");
-        url.href = games[i].links[ii].url;
-        url.target="_blank";
-        url.innerHTML = games[i].links[ii].label;
-        link.appendChild(url);
+                    let link = document.createElement("li");
+                    links.appendChild(link);
 
-    }
+                    let url = document.createElement("a");
+                    url.href = games[i].links[ii].url;
+                    url.target="_blank";
+                    url.innerHTML = games[i].links[ii].label;
+                    link.appendChild(url);
 
-    let link = document.createElement("li");
-    links.appendChild(link); 
+                }
 
-    let googleLink = document.createElement("a");
-    googleLink.href = `https://www.google.com/search?q=${games[i].title}, `
-    googleLink.target="_self";
-    googleLink.innerHTML = `Google: ${games[i].title}`;
-    link.appendChild(googleLink);
+                let link = document.createElement("li");
+                links.appendChild(link); 
 
-};
+                let googleLink = document.createElement("a");
+                googleLink.href = `https://www.google.com/search?q=${games[i].title}, `
+                googleLink.target="_self";
+                googleLink.innerHTML = `Google: ${games[i].title}`;
+                link.appendChild(googleLink);
 
-function getSteamData(endpoint, steam_user_id = "76561197995679405"){ // TODO: make this a more universal ajax function... accept ENDPOINT, STEAM_USER_ID
-    var request = new XMLHttpRequest();
-    request.open("POST", "/assets/proxy.php", true);
-    request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-    request.responseType = "json";
-    request.send("endpoint=" + endpoint + "&steam_user_id=" + steam_user_id);
-    request.onreadystatechange = function() {
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            // TODO: If not JSON (if first char != "{"), throw unexepected result error
-            switch(endpoint) {
-                case "GetPlayerSummaries":
-                    var data = this.response.response.players;
-                    break;
-                case "GetUserStatsForGame":
-                    var data = this.response.playerstats;
-                    break;
-                case "GetOwnedGames":
-                    var data = this.response.response.games;
-                    break;
-                case "GetRecentlyPlayedGames":
-                    var data = this.response.response.games;
-                    break;
-            }
-            console.log(endpoint, data);
-            // var gamesList = [];
-            // for(const game of games){
-            //     console.log(game);
-            //     var gameObj = {};
-            //     gameObj.appID = game.getElementsByTagName("appID")[0].textContent;
-            //     gameObj.name = game.getElementsByTagName("name")[0].textContent;
-            //     gameObj.logo = game.getElementsByTagName("logo")[0].textContent;
-            //     gameObj.storeLink = game.getElementsByTagName("storeLink")[0].textContent;
-            //     gameObj.statsLink = game.getElementsByTagName("statsLink").length > 0 ? game.getElementsByTagName("statsLink")[0].textContent : null;
-            //     gameObj.globalStatsLink = game.getElementsByTagName("globalStatsLink").length > 0 ? game.getElementsByTagName("globalStatsLink")[0].textContent : null;
-            //     gameObj.hoursOnRecord = game.getElementsByTagName("hoursOnRecord").length > 0 ? game.getElementsByTagName("hoursOnRecord")[0].textContent : null;
-            //     gameObj.hoursLast2Weeks = game.getElementsByTagName("hoursLast2Weeks").length > 0 ? game.getElementsByTagName("hoursLast2Weeks")[0].textContent : null;
-            //     gamesList.push(gameObj);
-            // }
-            // console.log(gamesList);
+            };
         }
-        // TODO: else, throw connection error
-    };
-    
-    
-}
-
-getSteamData("GetPlayerSummaries");
-getSteamData("GetUserStatsForGame");
-getSteamData("GetOwnedGames");
-getSteamData("GetRecentlyPlayedGames");
+    },
+    beforeMount(){
+        this.getSteamData("GetPlayerSummaries"),
+        this.getSteamData("GetUserStatsForGame");
+        this.getSteamData("GetOwnedGames");
+        this.getSteamData("GetRecentlyPlayedGames");
+     },
+     mounted(){
+         this.buildSteamGamesList();
+     }
+})
--- index.php
+++ index.php
@@ -29,7 +29,7 @@
 		<div class="container">
 			<div class="games"></div>
 			<div id="app">
-				{{ message }}
+				{{ games }}
 			</div>
 		</div>
 	</section>
