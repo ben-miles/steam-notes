b076b91721705088e2af2b21d415b0f19828dda5
Ben Miles <bcgm3studios@gmail.com>
2021-08-03 19:05:47 -0400

    Switch from "secret.php" to "config.php"
    Moving all my environment-specific variables to this more environment-specific-variable-specific named file.
    
    Also adding a variable for domain to complement the routing I just added.

---
+++ assets/config_example.php
@@ -0,0 +1,9 @@
+<?php
+
+// Host domain
+$domain = "your-domain-here";
+
+// For keeping my Steam API Key, and any other private info I may need
+$steam_api_key = "your-steam-api-key-here";
+
+?>
---
+++ template/header.php
@@ -0,0 +1,26 @@
+<!doctype html>
+
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<title>Ben's Steam Home Page</title>
+	<script src="https://unpkg.com/vue"></script>
+	<link rel="shortcut icon" type="image/png" href="assets/favicon.png"/>
+	<link rel="stylesheet" href="assets/style.css">
+
+</head>
+
+<body>
+
+	<section id="header">
+			<div class="container">
+				<div class="menu">
+					<a id="logo" href="https://store.steampowered.com/">
+						<img src="assets/steam_globalheader_logo.png" />
+					</a>
+					<a	href="https://google.com/">Google</a>
+				</div>
+			</div>
+	</section>
