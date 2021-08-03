660cd966f014d0ae6052dd45b539e0949c705836
Ben Miles <bcgm3studios@gmail.com>
2021-08-03 18:40:09 -0400

    Add The Simplest PHP Router
    My fav solution for PHP routing. https://www.taniarascia.com/the-simplest-php-router/

--- index.php
+++ index.php
@@ -1,3 +1,23 @@
+<?php
+
+// The Simplest PHP Router
+// https://www.taniarascia.com/the-simplest-php-router/
+
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '' :
+    case '/' :
+        require __DIR__ . '/views/home.php';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
+
+?>
+
 <!doctype html>
 
 <html lang="en">
