57c22accdbcdadc53d72bfc523bf6627a382814c
Ben Miles <bcgm3studios@gmail.com>
2021-09-01 02:26:37 -0400

    Add Meta for Responsive CSS

--- template/header.php
+++ template/header.php
@@ -4,6 +4,7 @@
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<title>Ben's Steam Home Page</title>
 	<script src="https://unpkg.com/vue"></script>
 	<link rel="shortcut icon" type="image/png" href="assets/favicon.png"/>
