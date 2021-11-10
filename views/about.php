42fd1bd89c64cbf46ddf73cd98a3661bc7a621da
Ben Miles <bcgm3studios@gmail.com>
2021-11-09 19:39:38 -0500

    Add View: About

---
+++ views/about.php
@@ -0,0 +1,19 @@
+<?php require "./template/header.php"; ?>
+
+<section class="body">
+	<div class="container">
+		<h2>What is Steam Notes?</h2>
+		<p>Steam Notes is a central location for notes, links, and reminders for your favorite games on on Steam.</p> 
+		<p>If you're like me, you bounce around between </p>
+		<h2>How does Steam notes work?</h2>
+		<ol>
+			<li><b>Sign in</b> with your Steam account (you will be redirected to Steam to sign in, then returned here).</li>
+			<li><b>Pin your games</b> to your home page to create and save notes.</li>
+			<li><b>Notes are automatically saved</b> everytime you add or remove a game, and as you type.</li>
+			<li><b>Your notes are backed up</b> to a database, so you can access them from any browser on any device.</li>
+		</ol>
+		<p><b>NOTE:</b> In order for this app to access your list of Steam games, your Steam Profile must be set to "Public." Go to your <a href="https://steamcommunity.com/my/edit/settings?snr=" target="_blank">Steam Profile Privacy Settings Page</a> and make sure that both "My profile" and "Game details" are set to "Public."</p>
+	</div>
+</section>
+
+<?php require "./template/footer.php"; ?>
