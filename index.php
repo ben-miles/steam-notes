e0fab5152980b3ed3233dfe3efc2a2285d43006a
Ben Miles <bcgm3studios@gmail.com>
2021-11-09 19:48:59 -0500

    Added New Ideas

--- index.php
+++ index.php
@@ -11,6 +11,7 @@ require 'assets/router.php';
 // - Add help view / ( tour? )
 // - Give visual/text feedback on DB save
 // - Textbox grows to fit input
+// - Save to localstorage, if not signed in
 
 // CHANGELOG
 // - v1 - 2021/08/28 - Brief abstract + features here
