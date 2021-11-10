16dc680e4ca71a25ba5e30d3b7288b8cd4bbd7d5
Ben Miles <bcgm3studios@gmail.com>
2021-11-09 19:47:55 -0500

    Routing: Home

--- assets/router.php
+++ assets/router.php
@@ -7,9 +7,13 @@ $request = $_SERVER['REQUEST_URI'];
 
 switch ($request) {
     case '' :
-    case '/' :
-        require './views/home.php';
-        break;
+	case '/' :
+		if(!isset($_SESSION['steamid'])) {
+			require './views/about.php';
+		} else {
+			require './views/home.php';
+		}
+		break;
     default:
         http_response_code(404);
         require './views/404.php';
