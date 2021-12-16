69172c9640d9b22928b0eae792a258f1d62c4b91
Ben Miles <bcgm3studios@gmail.com>
2021-12-16 00:51:58 -0500

    Fixed Typo

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 		<section id="footer">
-			© <?php echo date('Y'); ?> <a href="https://benmiles.com/" target="_blank">Ben Miles</a>. SteamNotes is not affiiated with Steam or Valve. <a href="mailto:bcgm3studios@gmail.com" target="_blank">Send bugs/feedback here!</a>
+			© <?php echo date('Y'); ?> <a href="https://benmiles.com/" target="_blank">Ben Miles</a>. SteamNotes is not affiliated with Steam or Valve. <a href="mailto:bcgm3studios@gmail.com" target="_blank">Send bugs/feedback here!</a>
 		</div>
 		</div>
 		<script type="text/javascript" src="<?php echo $domain; ?>/assets/script.js"></script>
